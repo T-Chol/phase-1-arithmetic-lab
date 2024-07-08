b34ac1904613c5578f91109b1758c134bf4924ae
T-Chol <taban.chol@student.moringaschool.com>
2024-07-08 17:00:30 +0300

    completes arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+//Write your code here
+var multiply;
+let num1 = 31;
+let num2 = 2;
+multiply =num1*num2;
+console.log (multiply);
 
-// Write your code here
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let mod;
+let num3 = 14;
+let num4 = 10;
+mod = num3%num4;
+
+let max;
+max = Math.max(1, 2, 20, 4, 5);
+console.log (max);
+
+// let sum = Math.random();
+// let sum1 = Math.floor(sum * 10) + 1;
+// console.log(sum1);
